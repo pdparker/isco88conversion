2d5024a7c52ce5e50a54a6d508da4e94c1a75b1b
Philip Parker <philip.parker@acu.edu.au>
2015-11-11 21:22:42 +1100

    Made some fixes based on R CMD check.

--- R/convert.R
+++ R/convert.R
@@ -1,5 +1,5 @@
 #USEAGE: convert(c("9321","9322","9330"), type = 'EGP')
-#x: int or char -> int
+#ISCO: int or char -> int
 #fromFormate: ISCO88. Can be other formats but will return first match rather than best match
 #to Format: can be EGP, SIOPS, ISEI. Will add ABS criteria soon
 #translator is a data frame consiting of conversion table
@@ -10,27 +10,27 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-convert <- function(x, type = "EGP", selfEmployed = NULL, unemployed = NULL){
+convert <- function(ISCO, type = "EGP", selfEmployed = NULL, unemployed = NULL){
 	require(hash)
 
-	if (type == "EGP" & length(selfEmployed) !=0 & x %in% selfEmployed){
+	if (type == "EGP" & length(selfEmployed) !=0 & ISCO %in% selfEmployed){
 		return(6)
 	}
-	else if (type == "EGP" & length(unemployed) !=0 & x %in% unemployed){
+	else if (type == "EGP" & length(unemployed) !=0 & ISCO %in% unemployed){
 		return(12)
 	}
 
-	else if(x %in% keys(ISCOhash) == FALSE){
+	else if(ISCO %in% keys(ISCOhash) == FALSE){
 		warning("One or more inputs is not a valid ISCO-88 code")
 		return(NA)
 	}
 
 	else{f1 <- function(type){
 		switch(type,
-			   EGP = ISCOhash[[as.character(x)]]$EGP,
-			   ISEI = ISCOhash[[as.character(x)]]$ISEI,
-			   SIOPS = ISCOhash[[as.character(x)]]$SIOPS,
-			   JOB = ISCOhash[[as.character(x)]]$JobDescription
+			   EGP = ISCOhash[[as.character(ISCO)]]$EGP,
+			   ISEI = ISCOhash[[as.character(ISCO)]]$ISEI,
+			   SIOPS = ISCOhash[[as.character(ISCO)]]$SIOPS,
+			   JOB = ISCOhash[[as.character(ISCO)]]$JobDescription
 		)
 	}
 	tmp <- unlist(f1(type))
